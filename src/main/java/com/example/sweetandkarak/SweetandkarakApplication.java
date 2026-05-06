616a18a4c2058a868df02cd5c8fd0a9e30090f9d
dawoodriaza <od.riaz@alsalambank.com>
2026-05-06 19:06:59 +0300

    Initial commit

---
+++ src/main/java/com/example/sweetandkarak/SweetandkarakApplication.java
@@ -0,0 +1,13 @@
+package com.example.sweetandkarak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetandkarakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetandkarakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sweetandkarak/SweetandkarakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sweetandkarak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetandkarakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
