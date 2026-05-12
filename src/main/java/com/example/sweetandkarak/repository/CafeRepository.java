6a1dcea206af658ae6e33bcbeb9c7aacb042e4b9
dawoodriaza <od.riaz@alsalambank.com>
2026-05-12 18:54:54 +0300

    Created Cafe Repository

---
+++ src/main/java/com/example/sweetandkarak/repository/CafeRepository.java
@@ -0,0 +1,8 @@
+package com.example.sweetandkarak.repository;
+
+import com.example.sweetandkarak.model.Cafe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CafeRepository extends JpaRepository<Cafe, Long> {
+
+}
